d07f9f2a3cced52de429a77148a3ca367364ec19
Alex <alex@MacBook-Pro-de-Alex.local>
2021-12-06 10:27:15 +0100

    pull alex

--- reactapp/src/pages/Documents.js
+++ reactapp/src/pages/Documents.js
@@ -1,14 +1,45 @@
 import React from "react";
 
 
-export default function Documents{
-    
-        return (
-            <div>
-                
+export default function Documents() {
+
+    return (
+        <View>
+            <div className="d-flex justify-content-center p-5">
+                <Dropdown toggle={function noRefCheck() { }}>
+                    <DropdownToggle caret>
+                        Dropdown
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem header>
+                            Header
+                        </DropdownItem>
+                        <DropdownItem>
+                            Some Action
+                        </DropdownItem>
+                        <DropdownItem text>
+                            Dropdown Item Text
+                        </DropdownItem>
+                        <DropdownItem disabled>
+                            Action (disabled)
+                        </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem>
+                            Foo Action
+                        </DropdownItem>
+                        <DropdownItem>
+                            Bar Action
+                        </DropdownItem>
+                        <DropdownItem>
+                            Quo Action
+                        </DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
             </div>
-        )
-    
+
+        </View>
+    )
+
 }
 
 
