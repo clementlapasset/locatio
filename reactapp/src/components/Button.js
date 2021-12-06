b84a5732f0ded59b4e999fbdcaa64afc4f91c557
Conor O'Malley <comalley88@gmail.com>
2021-12-06 14:19:41 +0100

    navbar composant

---
+++ reactapp/src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, NavbarBrand, Nav, NavLink, NavItem } from 'reactstrap';
+
+export default function NavBarHome() {
+    return (
+        <div>
+        <Button color="primary"> Click Me </Button>
+        </div>
+    )
+}
