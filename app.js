6ec82238f48ebb2df990386a640875fb46c5cf71
Klemo <clement.lapasset@gmail.com>
2021-12-10 09:26:47 +0100

    testFixHeroku7

--- app.js
+++ app.js
@@ -25,11 +25,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'reactapp/build')));
+//app.use(express.static(path.join(__dirname, 'reactapp/build')));
+
+app.use(express.static(path.resolve(__dirname, 'reactapp/build')));
+console.log(path.resolve(__dirname, 'reactapp/build'))
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, "reactapp/build/index.html"));
+  res.sendFile(path.resolve(__dirname, 'reactapp/build','index.html'));
 });
+console.log(path.resolve(__dirname, 'reactapp/build','index.html'))
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
