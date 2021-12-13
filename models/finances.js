fc067a0a89707e22350822e4b6dddc137db0534c
Klemo <clement.lapasset@gmail.com>
2021-12-13 11:00:42 +0100

    minor fix after conflicts

--- models/finances.js
+++ models/finances.js
@@ -9,7 +9,6 @@ const financeSchema = mongoose.Schema({
     frequence: Number,
     regulariserCharge: Number,
     regulariserProvision: Number,
-    Paiement: Number,
     propertyId: {type: mongoose.Schema.Types.ObjectId, ref: 'properties'}
 })
 
